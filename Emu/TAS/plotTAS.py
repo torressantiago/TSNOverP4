0d6a453f8aa08cfe337a9fb87b6597abee469f49
torressantiago <torres_santiago@javeriana.edu.co>
2022-02-28 17:45:02 +0100

    implemented analysis tools

--- Emu/TAS/plotTAS.py
+++ Emu/TAS/plotTAS.py
@@ -9,6 +9,8 @@ plt.rcParams['mathtext.rm'] = 'Bitstream Vera Sans'
 plt.rcParams['mathtext.it'] = 'Bitstream Vera Sans:italic'
 plt.rcParams['mathtext.bf'] = 'Bitstream Vera Sans:bold'
 
+
+# Interpreting received data
 data = pd.read_csv('../parsedpacketdata',sep='\s+',header=None)
 data = pd.DataFrame(data)
 
@@ -24,6 +26,46 @@ for i in range(len(x)):
 
 y = data[1]
 
+seconds = np.array(seconds)-seconds[0]
+port = y.to_numpy()
+
+normport1 = (port-6666)/1111
+normport2 = -(port-7777)/1111
+
+print(seconds)
+print(port)
+
+fig, (ax1,ax2) = plt.subplots(2)
+
+ax1.set_title("Packet reception timestamps (TAS)")    
+ax1.set_xlabel('Time [s]')
+ax1.set_ylabel('Packet Arrival')
+
+ax1.stem(seconds,normport1, 'C1-', label='PRI0',markerfmt='C1D')
+ax1.stem(seconds,normport2, 'C0-', label='PRI1',markerfmt='C0o')
+
+ax1.set_xlim([0, 0.2])
+
+leg = ax1.legend()
+
+# plt.show()
+
+# Interpreting sent data
+data = pd.read_csv('../parsedsenderoutput',sep='\s+',header=None)
+data = pd.DataFrame(data)
+
+x = data[1]
+x = x.values.tolist()
+
+date_time = seconds = x
+
+for i in range(len(x)):
+    date_time[i] = datetime.strptime(x[i], "%H:%M:%S.%f")
+    a_timedelta = date_time[i] - datetime(1900, 1, 1)
+    seconds[i] = a_timedelta.total_seconds()
+
+y = data[0]
+
 seconds = np.array(seconds)-seconds[1]
 port = y.to_numpy()
 
@@ -33,18 +75,15 @@ normport2 = -(port-7777)/1111
 print(seconds)
 print(port)
 
-fig = plt.figure()
-ax1 = fig.add_subplot(111)
+ax2.set_title("Packet emition timestamps (for TAS)")    
+ax2.set_xlabel('Time [s]')
+ax2.set_ylabel('Packet emition')
 
-ax1.set_title("Packet reception timestamps (TAS)")    
-ax1.set_xlabel('Time [s]')
-ax1.set_ylabel('Packet Arrival')
+ax2.stem(seconds,normport1, 'C1-', label='PRI0',markerfmt='C1D')
+ax2.stem(seconds,normport2, 'C0-', label='PRI1',markerfmt='C0o')
 
-ax1.stem(seconds,normport1, 'C1-', label='PRI0',markerfmt='C1D')
-ax1.stem(seconds,normport2, 'C0-', label='PRI1',markerfmt='C0o')
+ax2.set_xlim([0, 0.2])
 
-plt.xlim(right=0.2)
-
-leg = ax1.legend()
+leg = ax2.legend()
 
 plt.show()
